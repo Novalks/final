d2a546120e75a7da342b473c92debd1e1b2b8593
Novalks <116561876+Novalks@users.noreply.github.com>
2023-06-12 20:44:22 +0200

    Add files via upload

--- help.php
+++ help.php
@@ -4,6 +4,16 @@
 <link rel="stylesheet" href="models/styles.css">
 <button onclick="back()" class="btn myButton4 ">Back!</button>
 <div class="h1">Information!</div>
+<link rel="stylesheet" href="models/style1.css">
+            <div id="wrapper">
+                <div id="menu">
+
+                </div>
+                <div id="chatbox">
+                    <div id ="black">
+                    The website is designed for communication outside the game. To ask various questions about the game and communicate with other users.
+                    </div>
+                </div>
     <script>
     function back() {
                     window.location.href = "http://into.id.lv/ip19/arnis/galadarb/home.php";
